5fe89ce9fad5ee81aa0cf3d10ad5070ea64f0058
Zach <zheylmun@gmail.com>
2025-02-26 08:33:51 -0500

    Example cleanup

--- examples/steval-l99615c/src/bin/monitoring.rs
+++ examples/steval-l99615c/src/bin/monitoring.rs
@@ -1,5 +1,6 @@
 #![no_main]
 #![no_std]
+#![deny(warnings, unsafe_code)]
 
 use embassy_executor::Spawner;
 use embassy_time::Delay;
@@ -17,14 +18,14 @@ async fn main(_spawner: Spawner) -> ! {
     let config = Config {
         // Configure the voltage monitoring with extreme thresholds to avoid faults triggering
         voltage_thresholds: VoltageThresholds {
-            cell_over_voltage_threshold_mv: 2000,
+            cell_over_voltage_threshold_mv: 4200,
             cell_severe_over_voltage_delta_threshold_mv: 100,
-            cell_under_voltage_threshold_mv: 1000,
-            cell_severe_under_voltage_delta_threshold_mv: 500,
-            cell_balancing_under_voltage_delta_threshold_mv: 500,
-            pack_over_voltage_threshold_mv: 24000,
-            pack_under_voltage_threshold_mv: 0,
-            max_pack_cell_sum_delta_mv: 1000,
+            cell_under_voltage_threshold_mv: 2900,
+            cell_severe_under_voltage_delta_threshold_mv: 400,
+            cell_balancing_under_voltage_delta_threshold_mv: 600,
+            pack_over_voltage_threshold_mv: 20750,
+            pack_under_voltage_threshold_mv: 15000,
+            max_pack_cell_sum_delta_mv: 500,
             fault_counter_threshold: CounterThreshold::default(),
         },
         ..Default::default()
@@ -47,9 +48,9 @@ async fn main(_spawner: Spawner) -> ! {
         true,                // Enable cell 4 voltage measurement
         true,                // Enable cell 5 voltage measurement
         true,                // Enable Battery voltage measurement
-        false,               // Disable temperature measurement
-        true,                // Disable current measurement
-        true,                // Disable coulomb counter
+        true,                // Enable temperature measurement
+        true,                // Enable current measurement
+        true,                // Enable coulomb counter
         false,               // Disable over-current detection
         false,               // Disable short-circuit detection
         FetConfig::HighSide, // Set the discharge FET configuration to high-side
@@ -60,6 +61,7 @@ async fn main(_spawner: Spawner) -> ! {
 
     // Clear any faults that may have been triggered
     l9961.clear_all_faults().await.unwrap();
+    // Start the measurement loop
     l9961.enable_measurements().await.unwrap();
     let mut counter = 0;
     while counter < 100 {
